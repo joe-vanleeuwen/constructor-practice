12b665688aa9136dc2e0a5f018ea814257e198b0
Joe VanLeeuwen <joevanleeuwen@Joes-MacBook-Air.local>
2013-09-05 01:28:34 -0400

    add some specs and passed them. Changed the constructors around and implimented $.extend()

--- javascript.js
+++ javascript.js
@@ -14,9 +14,9 @@ function PirateShip (name, cannons, crew) {
     if(this.crew.length > 2) {
       var votes = [];
       // letting each mate vote. Using randomly generated number
-      this.crew.forEach(function(mate, index) {
-        if(mate.role.title !== "Captain") {
-          votes.push(mate.role.voteOnCaptain(this.crew.length));
+      this.crew.forEach(function(pirate, index) {
+        if(pirate.title !== "Captain") {
+          votes.push(pirate.voteOnCaptain(this.crew.length));
         } else {votes.unshift(true)};
       }, this);
 
@@ -40,19 +40,21 @@ function PirateShip (name, cannons, crew) {
             indexVal = index;
           };
         });
-        // making that pirate Captain and removing 
-        this.crew[indexVal].role = new Captain();
 
-        this.captain = this.crew[indexVal]
-        // this.add(new Pirate(this.crew[indexVal].name, new Captain()));
-        // this.crew.splice(indexVal, 1);
+        // for (prop in this.crew[indexVal]) { 
+        //   if (this.crew[indexVal].hasOwnProperty(prop)) {
+        //     if(prop !== "title" & prop !== "morale", prop !== "ship") {
+        //       delete this.crew[indexVal][prop]; 
+        //     }
+        //   }; 
+        // };
+
+        this.captain = changeAttrs(this.crew[indexVal], captainAttrs);
+        // this.crew[indexVal] = $.extend(this.crew[indexVal], captainAttrs);
+        // this.captain = this.crew[indexVal]
       };
     };
   };
-
-  // this.moraleStatus = 
-
-
 
   // this.attack = function(shipName) {
   //   var probability = 
@@ -65,102 +67,135 @@ function PirateShip (name, cannons, crew) {
 
 };
 
+function changeAttrs(pirate, newAttrs) {
+  for (prop in pirate) { 
+    if (pirate.hasOwnProperty(prop)) {
+      if(prop !== "name" & prop !== "morale" & prop !== "ship") {
+        delete pirate[prop]; 
+      }
+    }; 
+  };
+  pirate = $.extend(pirate, newAttrs);
+  return pirate;
+}
+
 // this would extend the object pirate to the additional captain properties
 // while preserving the original pirate object. this new object would be returned
 // as bob.
-bob = $.extend({}, pirate, captain);
+// bob = $.extend({}, pirate, captain);
 
 // removing the empty object argument, this would mutate pirate object.
-bob = $.extend(pirate, captain);
+// bob = $.extend(pirate, captain);
 
 // to delete properties from an  object
-for (prop in bob) { 
-  if (bob.hasOwnProperty(prop)) {
-   // add any extra exceptions with & prop !== "__"
-    if(prop !== "name") {
-      delete bob[prop]; 
-    }
-  }; 
-};
+// for (prop in bob) { 
+//   if (bob.hasOwnProperty(prop)) {
+//    add any extra exceptions with & prop !== "__"
+//     if(prop !== "title" & prop !== "morale") {
+//       delete bob[prop]; 
+//     }
+//   }; 
+// };
 
 
-function Pirate (name, role) {
+function Pirate (name) {
   this.name = name;
   this.morale = (Math.floor(Math.random() * (10)) + 1);
-  this.role = role;
 };
+
+// function Pirate (name) {
+//   this.name = name;
+//   this.morale = (Math.floor(Math.random() * (10)) + 1);
+//   this.title = "Mate",
+//   this.voteOnCaptain = function(numberOfMates) {
+//     return (Math.floor(Math.random() * (numberOfMates)) + 0);
+//   };
+//   this.swabTheDeck = function() {
+//     this.morale--;
+//   };
+// };
 
 // delete myJSONObject.regex;
 
 // mason
 
-captainAttrs = {
-  balbhalbhab: 'asgasgas',
-  killCrewMember: function(target){
+// captainAttrs = {
+//   balbhalbhab: 'asgasgas',
+//   killCrewMember: function(target){
     // kill someone here
-  }
-}
+//   }
+// }
 
 // $.extend(this.captain, captainAttrs)
 
 // end mason
 
 
-
-
-// roles of the pirates
-function Captain () {
-  this.title = "Captain"
-  this.walkPlankCommand = function(ship, name) {
-    ship.crew.forEach(function(pirate, index) {
+var captainAttrs = {
+  title: "Captain",
+  walkPlankCommand: function(name) {
+    this.ship.crew.forEach(function(pirate, index) {
       if (pirate.name === name) {
-        console.log(index)
-        ship.crew.splice(index, 1);
+        pirate.ship.crew.splice(index, 1);
       };
     });
-  };
+  },
 
-  var firstMateExists = false
-  this.chooseFirstMate = function(ship) {
-      var x = 0;
-      var indexVal = 0;
-      if(!firstMateExists) {
-        ship.crew.forEach(function(pirate, index) {
-          if (pirate.morale > x) {
-            if(pirate.role.title !== "Captain") {
-              x = pirate.morale;
-              indexVal = index;
-            };
-          };
-        });
-        ship.crew[indexVal].role = new FirstMate();
+  chooseFirstMate: function() {
+    var x = 0;
+    var indexVal = 0;
+    var firstMateExists = false;
+    // test to see if a First Mate already exists
+    this.ship.crew.forEach(function(pirate) {
+      if (pirate.title === "First Mate") {
         firstMateExists = true;
-      };
-  };
-  this.swabDeckCommand = function() {
+      }
+    });
+    // if First Mate does not exist, assign Mate with highest morale to be First Mate
+    if(!firstMateExists) {
+      this.ship.crew.forEach(function(pirate, index) {
+        if (pirate.morale > x) {
+          if(pirate.title !== "Captain") {
+            x = pirate.morale;
+            indexVal = index;
+          };
+        };
+      });
 
-  };
+      this.ship.firstMate = changeAttrs(this.ship.crew[indexVal], firstMateAttrs)
+    };
+  },
+  swabDeckCommand: function() {
+    this.ship.crew.forEach(function(pirate) {
+      if (pirate.title === "Mate") {
+        console.log("Before: " + pirate.title + ": " + pirate.morale)
+        pirate.swabTheDeck();
+        console.log("After: " + pirate.title + ": " + pirate.morale)
+      }
+    });
+  }
 };
 
-function FirstMate () {
-  this.title = "First Mate"
-  this.attemptMutiny = function(ship) {
+
+var firstMateAttrs = {
+  title: "First Mate",
+  attemptMutiny: function() {
     var shipMorale = 0;
     var captainIndex;
     var firstMateIndex;
     // chance of zero will never achieve successful mutiny
     var chance = 1;
-    ship.crew.forEach(function(pirate, index) {
-      if (pirate.role.title === "Captain") {
+    this.ship.crew.forEach(function(pirate, index) {
+      if (pirate.title === "Captain") {
         captainIndex = index
       } 
-      else if(pirate.role.title === "First Mate") {
+      else if(pirate.title === "First Mate") {
         firstMateIndex = index;
         shipMorale += pirate.morale;
       }
       else {shipMorale += pirate.morale};
     });
-    shipMorale = Math.floor(shipMorale/(ship.crew.length - 1));
+    shipMorale = Math.floor(shipMorale/(this.ship.crew.length - 1));
 
     if (shipMorale <= 5) {
       chance = 2
@@ -170,52 +205,157 @@ function FirstMate () {
     }
 
     if((Math.floor(Math.random() * (chance)) + 1) > 1) {
-      ship.crew[firstMateIndex].role = new Captain();
-      ship.crew.splice(captainIndex, 1);
+      this.ship.crew.captain = changeAttrs(this.ship.crew[firstMateIndex], captainAttrs)
+      this.ship.crew.splice(captainIndex, 1);
     }
-  };
+  }
 };
 
-function Mate () {
-  this.title = "Mate"
-  this.voteOnCaptain = function(numberOfMates) {
+var mateAttrs = {
+  title: "Mate",
+  voteOnCaptain: function(numberOfMates) {
     return (Math.floor(Math.random() * (numberOfMates)) + 0);
-  };
-  this.swabTheDeck = function() {
+  },
+  swabTheDeck: function() {
     this.morale--;
-  };
+  }
 };
 
 hunter = new PirateShip()
-// hunter.add(new Pirate("Steve", new Captain()))
-// hunter.add(new Pirate("Genius", new FirstMate()))
-hunter.add(new Pirate("Steve", new Mate()))
-hunter.add(new Pirate("Genius", new Mate()))
-hunter.add(new Pirate("Guy", new Mate()))
-hunter.add(new Pirate("Dude", new Mate()))
-
-// creates dude the Pirate with the role of Captain
-
-// dude = new Pirate((new Captain());
-
-// dude = new Pirate(Captain)
-
-// so make all the Pirates mates. Give them the method 'vote'
-// so they can vote on who should be captain. 
-// 
-// Assign each mate'svote to a random number.
-// 
-// Give role of captain to winner.
-// 
+hunter.add($.extend(new Pirate("Bob"), mateAttrs));
+hunter.add($.extend(new Pirate("Dude"), mateAttrs));
+hunter.add($.extend(new Pirate("Steve"), mateAttrs));
+hunter.add($.extend(new Pirate("Bill"), mateAttrs));
 
 
+// helping Keeron
+
+// function formPack(type) {
+//   var count = 0;
+//     animalCollection.animals.forEach(function(animal) {
+//         if (anmial.type === type) {
+//           count++;
+//         }
+//     });
+//     if (count > 5) {
+//       var (type + "Pack") = [];
+//           animalCollection.animals.forEach(function(animal) {
+//         if (anmial.type === type) {
+//           ourPackArray.push(animal);
+//         }
+//     });
+//     }
+// }
 
 
+// function AnimalCollection() {
+//     this.animals = [];
+//     this.allPacks = [];
+
+//     this.add = function(newAnimal) {
+//         this.animals.push(newAnimal)
+//     };
+
+//     this.formPack = function(type) {
+//         var count = 0;
+//         this.animals.forEach(function(animal) {
+//             console.log(animal)
+//             if (animal.type === type) {
+//               count++;
+//               console.log(count)
+//             }
+//         });
+//         if (count > 5) {
+//           var pack = {type: []};
+//               this.animals.forEach(function(animal) {
+//                 console.log(animal)
+//                 if (animal.type === type) {
+//                    pack.type.push(animal);
+//                 }
+//             });
+//         };
+//         this.allPacks.push(pack);
+//     };    
+// };
 
 
+// function AnimalCollection() {
+//     this.animals = [];
+//     this.allPacks = [];
+
+//     this.add = function(newAnimal) {
+//         this.animals.push(newAnimal)
+//     };
+
+//     this.formPack = function() {
+//         var packType = "";
+//         this.animals.forEach(function(animal) {
+//             this.allPacks.forEach(function(pack) {
+//                 if (pack.type !== animal.type) {
+//                     packType = animal.type;
+//                     console.log(packType)
+//                 }
+//             });
+//             var count = 0;
+//             this.animals.forEach(function(animal) {
+//                 console.log(animal)
+//                 if (animal.type === packType) {
+//                     count++;
+//                     console.log(count)
+//                 };
+//             });
+//             if (count > 5) {
+//               var pack = {packType: []};
+//                   this.animals.forEach(function(animal) {
+//                     console.log(animal)
+//                     if (animal.type === packType) {
+//                        pack.type.push(animal);
+//                     }
+//                 });
+//             };
+//             this.allPacks.push(pack);
+//         }, this);
+//     };    
+// };
+//     this.formPack = function() {
+//         var packType = "";
+//         this.animals.forEach(funciton(animal) {
+//             this.allPacks.forEach(function(pack) {
+//                 if (pack.type !== animal.type) {
+//                     packType = animal.type;
+//                 }
+//             }, this);
+//           var count = 0;
+//             this.animals.forEach(function(animal) {
+//                 if (anmial.type === packType) {
+//                   count++;
+//                 }
+//             });
+//             if (count > 5) {
+//               var pack = {packType: []};
+//                   this.animals.forEach(function(animal) {
+//                 if (anmial.type === packType) {
+//                   pack.push(animal);
+//                 }
+//                 });
+//             }
+//             allPacks.push(pack);
+//         }, this);
+//     };
+// }
 
 
+// function Animal(type) {
+//     this.type = type;
+// }
 
+// var SafariAnimals = new AnimalCollection()
+// SafariAnimals.add(new Animal('Lion'));
+// SafariAnimals.add(new Animal('Lion'));
+// SafariAnimals.add(new Animal('Lion'));
+// SafariAnimals.add(new Animal('Lion'));
+// SafariAnimals.add(new Animal('Lion'));
+// SafariAnimals.add(new Animal('Lion'));
 
 
 
--- spec/constructor-specs.js
+++ spec/constructor-specs.js
@@ -1,8 +1,8 @@
 describe("the constructor, ", function() {
 
-	describe("PirateShip", function() {
+	theHorizonDrifter = new PirateShip();
 
-		theHorizonDrifter = new PirateShip();
+	describe("PirateShip", function() {
 
 		beforeEach(function() {
 			theHorizonDrifter.crew = [];
@@ -26,27 +26,40 @@ describe("the constructor, ", function() {
 			expect(theHorizonDrifter.crew.length).toBe(0);
 		});
 
-		it(".push() should add one to crew array", function() {
+		it(".add() should add one to crew array", function() {
 
 			var length = theHorizonDrifter.crew.length
-			theHorizonDrifter.add(new Pirate("Bob", new Mate))
+			theHorizonDrifter.add($.extend(new Pirate("Bob"), mateAttrs));
 			expect(theHorizonDrifter.crew.length).toBe(length + 1);
+		});
+
+		it(".add() should add one to crew array who has the property .swabTheDeck", function() {
+
+			theHorizonDrifter.add($.extend(new Pirate("Bob"), mateAttrs));
+			expect("swabTheDeck" in theHorizonDrifter.crew[0]).toBe(true);
 		});
 
 		it(".chooseCaptain() should not make a Mate captain with less than 3 mates onboard", function() {
 
-			theHorizonDrifter.add(new Pirate("Bob", new Mate))
-			theHorizonDrifter.add(new Pirate("Bob", new Mate))
+			theHorizonDrifter.add($.extend(new Pirate("Bob"), mateAttrs));			
+			theHorizonDrifter.add($.extend(new Pirate("Bob"), mateAttrs));			
 			theHorizonDrifter.chooseCaptain()
 
 			var captainExists = false;
 			theHorizonDrifter.crew.forEach(function(pirate) {
-				if(pirate.role.title === "Captain") {
+				if(pirate.title === "Captain") {
 					captainExists = true;
 				}
 			});
 
 			expect(captainExists).toBe(false);
+		});
+
+		it(".chooseCaptain() not remove the pirate's name property when using $.extend()", function() {
+
+			theHorizonDrifter.add($.extend(new Pirate("Bob"), mateAttrs));	
+			theHorizonDrifter.captain = changeAttrs(theHorizonDrifter.crew[0], captainAttrs);
+			expect(theHorizonDrifter.captain.name).toBe("Bob");
 		});
 
 	});
@@ -60,33 +73,87 @@ describe("the constructor, ", function() {
 		});
 
 
-		describe("Captain", function() {
+		describe("with captainAttrs", function() {
+
+			beforeEach(function() {
+				theHorizonDrifter.crew = [];
+				theHorizonDrifter.add($.extend(new Pirate("Dude"), captainAttrs));	
+				theHorizonDrifter.add($.extend(new Pirate("Guy"), mateAttrs));	
+				theHorizonDrifter.add($.extend(new Pirate("Steve"), mateAttrs));	
+				theHorizonDrifter.add($.extend(new Pirate("Genius"), mateAttrs));	
+				theHorizonDrifter.add($.extend(new Pirate("Adam"), mateAttrs));
+				theHorizonDrifter.crew[0].morale = 11;
+				theHorizonDrifter.crew[1].morale = 8;
+				theHorizonDrifter.crew[2].morale = 10;
+				theHorizonDrifter.crew[3].morale = 3;
+				theHorizonDrifter.crew[4].morale = 7;
+
+				theHorizonDrifter.captain = theHorizonDrifter.crew[0];
+			});
+
+			it(".walkPlankCommand() should remove 1 Mate from crew", function() {
+
+				theHorizonDrifter.crew = [];
+				
+				theHorizonDrifter.add($.extend(new Pirate("Dude"), captainAttrs));	
+				theHorizonDrifter.add($.extend(new Pirate("Guy"), mateAttrs));	
+				var length = theHorizonDrifter.crew.length;
+
+				theHorizonDrifter.captain.walkPlankCommand("Guy");
+				expect(theHorizonDrifter.crew.length).toBe(length - 1);
+			});
+
+			it(".chooseFirstMate() should promote the pirate with the highest morale to FirstMate", function() {
+
+				theHorizonDrifter.captain.chooseFirstMate();
+				expect(theHorizonDrifter.firstMate.name).toBe("Steve");
+			});
+
+			it(".chooseFirstMate() should not promote additional pirates to First Mate when one already exists", function() {
+
+				theHorizonDrifter.captain.chooseFirstMate();
+				theHorizonDrifter.captain.chooseFirstMate();
+				theHorizonDrifter.captain.chooseFirstMate();
+				theHorizonDrifter.captain.chooseFirstMate();
+
+				expect(theHorizonDrifter.crew[1].title).not.toBe("First Mate");
+				expect(theHorizonDrifter.crew[3].title).not.toBe("First Mate");
+				expect(theHorizonDrifter.crew[4].title).not.toBe("First Mate");
+			});
+
+			it(".chooseFirstMate() should not promote the Captain to First Mate regardless if he has highest morale points", function() {
+
+				theHorizonDrifter.captain.chooseFirstMate();
+
+				expect(theHorizonDrifter.crew[0].title).not.toBe("First Mate");
+			});
+			
+
+			it("swabDeckCommand() should reduce all mates' morale points by 1", function() {
+
+				theHorizonDrifter.add($.extend(new Pirate("Guy"), firstMateAttrs));	
+				theHorizonDrifter.crew[5].morale = 5;
+				theHorizonDrifter.captain.swabDeckCommand();
+
+				expect(theHorizonDrifter.crew[1].morale).toBe(7);
+				expect(theHorizonDrifter.crew[2].morale).toBe(9);
+				expect(theHorizonDrifter.crew[3].morale).toBe(2);
+				expect(theHorizonDrifter.crew[4].morale).toBe(6);
+				expect(theHorizonDrifter.crew[5].morale).toBe(5);
+
+			});
+
+		});
+
+		describe("with firstMateAttrs", function() {
 
 			beforeEach(function() {
 				theHorizonDrifter.crew = [];
 			});
 
-			it(".role.walkPlankCommand() should remove 1 Mate from crew", function() {
-				
-				theHorizonDrifter.add(new Pirate("Dude", new Captain()));
-				theHorizonDrifter.add(new Pirate("Guy", new Mate()));
-				var length = theHorizonDrifter.crew.length;
-
-				theHorizonDrifter.crew[0].role.walkPlankCommand(theHorizonDrifter, "Guy");
-				expect(theHorizonDrifter.crew.length).toBe(length - 1);
-			});
-
-			it(".role.chooseFirstMate() should return a (randomly) promoted Mate who now is FirstMate", function() {
-				
-				theHorizonDrifter.add(new Pirate("Dude", new Captain()));
-				theHorizonDrifter.add(new Pirate("Guy", new Mate()));
-
-				theHorizonDrifter.crew[0].role.chooseFirstMate(theHorizonDrifter);
-				expect(theHorizonDrifter.crew[1].role.title).toBe("First Mate");
-			});
 		});
 
-		describe("FirstMate", function() {
+		describe("with mateAttrs", function() {
 
 			beforeEach(function() {
 				theHorizonDrifter.crew = [];
@@ -94,27 +161,20 @@ describe("the constructor, ", function() {
 
 			it("should return an object who's constructor is Priate", function() {
 
-				firstMateBob = new Pirate(new FirstMate());
+				cutlassJack = ($.extend(new Pirate("Guy"), mateAttrs));	
 				expect(cutlassJack.constructor).toBe(Pirate);
 			});
-		});
 
-		describe("Mate", function() {
+			it(".title should return correct title", function() {
 
-			beforeEach(function() {
-				theHorizonDrifter.crew = [];
-			});
-
-			it(".role.title should return correct title", function() {
-
-				theHorizonDrifter.add(new Pirate("Guy", new Mate()));
-				expect(theHorizonDrifter.crew[0].role.title).toBe("Mate");
+				theHorizonDrifter.add($.extend(new Pirate("Guy"), mateAttrs));
+				expect(theHorizonDrifter.crew[0].title).toBe("Mate");
 			});
 
 			it("voteOnCaptain() should return a random number between 0 and theHorizonDrifter.crew.length", function() {
 
-				theHorizonDrifter.add(new Pirate("Dude", new Mate()));
-				var x = theHorizonDrifter.crew[0].role.voteOnCaptain(theHorizonDrifter.crew.length);
+				theHorizonDrifter.add($.extend(new Pirate("Guy"), mateAttrs));
+				var x = theHorizonDrifter.crew[0].voteOnCaptain(theHorizonDrifter.crew.length);
 
 				expect(x).toBe(0);
 			});
